378bd2863db0181a4ce16732f03a97cee250b2a2
Md-Sifatullah617 <sifatullah35-617@diu.edu.bd>
2025-01-14 19:28:42 +0600

    first commit

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Potential Gallery'),
+      ),
+      body: const Center(
+        child: Text(
+          'Welcome to the Potential Gallery!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
